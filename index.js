7a2d13d95e3d0c0713986f9ea0128d787370707b
AlexBardadym <alex.bardadym1990@gmail.com>
2019-01-26 18:56:09 +0200

    3rd commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function showMenu() {
+    var h = document.getElementById('hidenmenu').style;
+    h.display = "none";
+    var m = document.getElementById('showmenu').style;
+    m.display = "flex";
+    var a = document.getElementById('menutablet').style;
+    a.height = "100px";
+}
